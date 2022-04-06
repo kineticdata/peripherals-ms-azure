251f83f7d50aed99d6fb06923deb029d07a5448a
Chad Rehm <chad.rehm@kineticdata.com>
2022-04-06 14:51:58 -0500

    PER-240 created a queue message handler The handler can preform all CRUD operations available to Queue Message endpoint.

--- handlers/azure_queue_messages_api_v1/handler/init.rb
+++ handlers/azure_queue_messages_api_v1/handler/init.rb
@@ -35,6 +35,7 @@ class AzureQueueMessagesApiV1
     @path = @parameters["path"]
     @path = "/#{@path}" if !@path.start_with?("/")
 
+    @accept = 'text/xml'
     @content_type = 'text/xml'
   end
 
@@ -62,7 +63,7 @@ class AzureQueueMessagesApiV1
           "grant_type" => "client_credentials",
           "scope" => "https://#{@storage_account}.queue.core.windows.net/.default"
         }, \
-        headers: {:content_type => 'application/x-www-form-urlencoded'}
+        headers: {:content_type => 'application/x-www-form-urlencoded', :accept => @accept}
        
       # Parse token from request
       puts "Access token retrieved." if @debug_logging_enabled
@@ -84,7 +85,8 @@ class AzureQueueMessagesApiV1
         url: asset_url, \
         payload: encoded_body, \
         headers: {
-          :content_type => @content_type,
+          :content_type => @content_type, 
+          :accept => @accept,
           :Authorization => "Bearer #{token}",
           'x-ms-date' => Time.now.utc.strftime("%a, %d %b %Y %H:%M:%S GMT"),
           'x-ms-version' => "2017-11-09"
--- handlers/azure_queue_messages_api_v1/readme.md
+++ handlers/azure_queue_messages_api_v1/readme.md
@@ -25,7 +25,6 @@ Get four messages for the <foo> queue
     'error_handling' => 'Error Message',
     'headers' => '',
     'method' => 'GET',
-    'storage_account' => '<account name>',
     'path' => '/<foo>/messages?numofmessages=4',
     'body' => ''
   }
@@ -36,7 +35,6 @@ Delete a message for the <foo> queue
     'error_handling' => 'Error Message',
     'headers' => '',
     'method' => 'DELETE',
-    'storage_account' => '<account name>',
     'path' => '/<foo>/messages/<message id>?popreceipt=<pop receipt id>',
     'body' => ''
   }
@@ -47,7 +45,6 @@ Update an existing message for the <foo> queue
     'error_handling' => 'Error Message',
     'headers' => '',
     'method' => 'PUT',
-    'storage_account' => '<account name>',
     'path' => '/<foo>/messages/<message id>?popreceipt=<pop receipt id>',
     'body' => 'Update to message'
   }
